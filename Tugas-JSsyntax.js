b4156dea5291a04d83d40faaaaa2407a262f945c
Andhika Wicaksono PF <git@github.com:AndhikaWicaksonoPF>
2023-05-03 22:54:04 +0700

    Tugas JS Syntax

---
+++ Tugas-JSsyntax.js
@@ -0,0 +1,11 @@
+let angka = 10;
+let tulisan = "saya belajar javascript";
+let hasil = angka + tulisan;
+
+// Cek apakah hasil adalah string
+if (typeof hasil === "string") {
+  let angkaKetiga = 7;
+  hasil = angkaKetiga + tulisan;
+}
+
+console.log(hasil);
